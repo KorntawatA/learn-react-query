87a5e97a244dd7a6f317af923eb4be6f7d7f3e17
KorntawatA <korntawat.a@gmail.com>
2023-08-16 17:41:21 +0700

    Tidy up code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import ReactQuery from './components/ReactQuery'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 const queryClient = new QueryClient()
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQuery />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   )
 }
--- src/components/ReactQuery.jsx
+++ src/components/ReactQuery.jsx
@@ -1,28 +1,36 @@
 import React from "react";
-import { useQuery } from "react-query";
+import { useQuery } from '@tanstack/react-query'
 
 export default function ReactQuery() {
-  const [pokemon, setPokemon] = React.useState('')
-  async function getPokemon() {
-    return await (await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)).json()
+  const [input, setInput] = React.useState('')
+  const getPokemon = async ({ queryKey }) => {
+    const [_key, { input: pokemon }] = queryKey
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    const responseFormatted = response.json()
+    return responseFormatted
   }
-  const { data, isLoading, isFetching, refetch } = useQuery("pokemon", getPokemon, {
-    enabled: false
+  const { data, isLoading, isFetching, refetch } = useQuery({
+    queryKey: ["pokemon", { input }],
+    queryFn: getPokemon,
+    enabled: false,
   })
-  console.log({isLoading, isFetching})
+
+  React.useEffect(() => {
+    getPokemon()
+  }, [])
   
   return(
     <div style={{display: "flex", flexDirection: "column"}}>
-      <input type="text" onChange={e => setPokemon(e.target.value)} />
+      <input type="text" onChange={e => setInput(e.target.value)} />
       <button onClick={() => refetch()}>GET POKEMON</button>
       {isFetching || isLoading
         ? 
-        <div>Something is happening...</div>
+        <p>Something is happening...</p>
         :
         <div>
-          <div>{JSON.stringify(data?.name)}</div>
-          <div>{JSON.stringify(data?.id)}</div>
-          <div>{JSON.stringify(data?.types)}</div>
+          <p>{JSON.stringify(data?.name)}</p>
+          <p>{JSON.stringify(data?.id)}</p>
+          <p>{JSON.stringify(data?.types)}</p>
         </div>
       }
     </div>
