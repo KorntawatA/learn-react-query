f1cd1358e634c674f4c1b65be6a7b2efa60ee89f
KorntawatA <korntawat.a@gmail.com>
2023-08-17 10:26:42 +0700

    Automatic query upon input changes

--- src/components/ReactQuery.jsx
+++ src/components/ReactQuery.jsx
@@ -6,23 +6,17 @@ export default function ReactQuery() {
   const getPokemon = async ({ queryKey }) => {
     const [_key, { input: pokemon }] = queryKey
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    const responseFormatted = response.json()
+    const responseFormatted = await response.json()
     return responseFormatted
   }
-  const { data, isLoading, isFetching, refetch } = useQuery({
+  const { data, isLoading, isFetching } = useQuery({
     queryKey: ["pokemon", { input }],
     queryFn: getPokemon,
-    enabled: false,
   })
-
-  React.useEffect(() => {
-    getPokemon()
-  }, [])
   
   return(
     <div style={{display: "flex", flexDirection: "column"}}>
       <input type="text" onChange={e => setInput(e.target.value)} />
-      <button onClick={() => refetch()}>GET POKEMON</button>
       {isFetching || isLoading
         ? 
         <p>Something is happening...</p>
